fcb080d7b627f1bfca21a66f1a89f321b395ab00
unknown <f.carrara24@studenti.unibg.it>
2023-11-10 16:32:41 +0100

    all immages are passed on the cards using axios

---
+++ src/components/CardApp.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card gap-2 d-flex flex-column justify-content-start" @click="logItems(image)">
+        <div class="image-conteiner">
+            <img :src="image" alt="">
+        </div>
+        <div class="title">
+            {{ title }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: String,
+        cost: String,
+        title: String,
+        typology: String
+    },
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+        this.logItems(this.image)
+    },
+    methods: {
+        logItems(val) {
+            console.log('image ' + val)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,18 +1,43 @@
 <template>
     <div class="container">
         <h1>{{ content }}</h1>
+        <CardApp v-for="card in importedFiles" :image="card.thumb" :cost="card.price" :title="card.series"
+            :typology="card.type" />
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import CardApp from './CardApp.vue';
 export default {
     name: 'MainApp',
+    components: {
+        CardApp
+    },
     data() {
-
-
         return {
-            content: '-->Content goes here<--'
+            content: '-->Content goes here<--',
+            importedFiles: undefined
         }
+    },
+    methods: {
+        fetchData() {
+            axios
+                .get('../src/components/data/dc-comics.json')
+                .then(response => {
+                    this.importedFiles = response.data
+                    console.log(this.importedFiles)
+                })
+        },
+        ParseData() {
+
+        }
+    },
+    mounted() {
+
+        this.fetchData()
+
+
     }
 }
 </script>
